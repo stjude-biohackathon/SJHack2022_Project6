3c56d33b9e30cb2fa8e7e87f1dbd4caf7940e8e0
kedusi <kedusi@gmail.com>
2022-04-11 17:46:33 -0500

    space-around in nav flex

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -5,8 +5,8 @@ const Nav = () => {
 
 
     return (
-		<nav class='w-full flex items-center justify-between flex-wrap bg-red-900 p-6'>
-			<div class='flex items-center flex-shrink-0 text-white mr-6'>
+		<nav class='w-full flex items-center justify-around flex-wrap bg-red-900 p-6'>
+			<div class='flex items-center shrink-0 text-white mr-6'>
 				<Logo title='St. Jude child logo' />
 				<span class='font-semibold text-xl tracking-tight'>
 					Simply Samples
@@ -29,6 +29,8 @@ export default Nav
 
 
 /**
+ * tailwind example used
+ * 
  * <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
     <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
